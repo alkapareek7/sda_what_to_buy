8f7f35157c916e11e9200f296da3a1e00c5a51dc
alka <alkapareek7@gamil.com>
2023-02-18 12:36:59 +0200

    reverted to initial state

--- whatToBuyCore/items_list/urls.py
+++ whatToBuyCore/items_list/urls.py
@@ -1,11 +1,9 @@
 
 from django.urls import path, include
-from .views import my_lists, items_list, ItemCrateView, ItemDeleteView, ItemUpdateView
+from .views import my_lists, items_list, 
 
 urlpatterns = [
     path("my_lists/", my_lists, name="lists-list"),
-    path("my_lists/<pk>", items_list, name= "items-list"),
-    path("item/add/", ItemCrateView.as_view(), name= 'item-add'),
-    path("item/<int:pk>/update/", ItemUpdateView.as_view(), name="item-update"),
-    path("item/<int:pk>/delete/", ItemDeleteView.as_view(), name="item-delete")
+    path("my_lists/<pk>/", items_list, name= "items-list"),
+    
 ]
--- whatToBuyCore/items_list/views.py
+++ whatToBuyCore/items_list/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ShoppingList, ShoppingItem
-from .models import ShoppingList
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
 from django.urls import reverse_lazy
 
-
-class ItemCrateView(CreateView):
-    model = ShoppingItem
-    fields = '__all__'
-
-class ItemUpdateView(UpdateView):
-    model = ShoppingItem
-    fields = '__all__'
-
-class ItemDeleteView(DeleteView):
-    model = ShoppingItem
-    success_url = reverse_lazy("lists_list")
 
     
 def my_lists(request):
